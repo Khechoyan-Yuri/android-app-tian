2a661f78022aa526a222b3b7dd080e66051f31f1
Jessie-Wilkins <wilkinsj@fontbonne.edu>
2017-04-11 18:52:13 -0500

    First push of the code

---
+++ DunDeal/app/src/main/java/com/example/jessie/dundeal/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.jessie.dundeal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.activity_main);
+
+    }
+
+    public void CreateNewText() {
+        view.setText("Get me a coffee");
+        view.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+
+    }
+}
