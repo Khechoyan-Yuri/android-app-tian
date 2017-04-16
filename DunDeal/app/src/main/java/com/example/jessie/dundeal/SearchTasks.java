55a11702f5d520b390dd2082c6b623d67ddf442d
sspectra <tenshivess@gmail.com>
2017-04-15 20:41:34 -0500

    Task search Java file

---
+++ DunDeal/app/src/main/java/com/example/jessie/dundeal/SearchTasks.java
@@ -0,0 +1,45 @@
+package com.example.jessie.dundeal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class SearchTasks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_tasks);
+
+        View convertView;
+
+        //essentially search database & populate the page, doing this in a loop for every result
+        //we can probably set this up like the ViewAdapter hw if we want more efficiency
+        //but the priority right now is completion, not perfection
+        LayoutInflater inflater;
+        LinearLayout activity_search_tasks = (LinearLayout) findViewById(R.id.RL_activity_search_tasks);
+        //for (int i = 0; i < options.length; i++) { //for each of the items in the database
+        inflater = getLayoutInflater();
+        convertView = inflater.inflate(R.layout.box, null);
+        activity_search_tasks.addView(convertView);
+
+        TextView txt = (TextView) findViewById(R.id.box_subtitle1);
+        txt.setText("Example changed title");//example of changin title as we would when fetching from database
+        //We would also place an onclick method here for the buttons, which would take us to appropriate details
+        //and add the task to accepted tasks
+
+    }
+
+
+    public void GoBack(View v) {
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+
+        startActivity(i);
+    }
+}
