37baaac5b168d1888b878ef8bf3acb8db06b8e37
sspectra <tenshivess@gmail.com>
2017-04-16 19:24:39 -0500

    Code edits to allow dynamically adding tasks/requests

--- DunDeal/app/src/main/java/com/example/jessie/dundeal/MainActivity.java
+++ DunDeal/app/src/main/java/com/example/jessie/dundeal/MainActivity.java
@@ -18,6 +18,45 @@ public class  MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         CreateText();
+        
+         View convertView;
+        LayoutInflater inflater;
+        TextView txt;
+
+        //essentially search database & populate the page, doing this in a loop for every result
+        //we can probably set this up like the ViewAdapter hw if we want more efficiency
+        //but the priority right now is completion, not perfection
+        for(int i = 0; i < 4; i++) {//do for every task
+            //get task info from database here
+            LinearLayout tasks = (LinearLayout) findViewById(R.id.LL_tasks);
+            //for (int i = 0; i < options.length; i++) { //for each of the items in the database
+            inflater = getLayoutInflater();
+            convertView = inflater.inflate(R.layout.box, null);
+            tasks.addView(convertView);
+
+            //must use convertView.findViewById to fetch dynamically, or else all edits apply to one item
+            txt = (TextView) convertView.findViewById(R.id.box_subtitle1);
+            //txt.setId(55+1);//for some reason doesn't accept singular numbers?
+            txt.setText("Example changed title" + i);//example of changing title as we would when fetching from database
+            //We would also place an onclick method here for the buttons, which would take us to appropriate details
+            //and add the task to accepted tasks
+         }//end for loop
+
+        //essentially do the same thing for the requests
+        //for(int i = 0; i < number_of_tasks; i++) {//do for every task
+        //get task info from database here
+        LinearLayout requests = (LinearLayout) findViewById(R.id.LL_requests);
+        //for (int i = 0; i < options.length; i++) { //for each of the items in the database
+            inflater = getLayoutInflater();
+            convertView = inflater.inflate(R.layout.box, null);
+            requests.addView(convertView);
+
+            //must use convertView.findViewById to fetch dynamically, or else all edits apply to one item
+            txt = (TextView) convertView.findViewById(R.id.box_subtitle1);
+            txt.setText("Example changed title2");//example of changing title as we would when fetching from database
+            //We would also place an onclick method here for the buttons, which would take us to appropriate details
+            //and add the task to accepted tasks
+        // }//end for loop
 
     }
 
